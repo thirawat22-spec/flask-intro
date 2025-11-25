4a563d89ed814bbec7b536b5f02da63bed53850c
thirawat <thirawat7054@gmail.com>
2025-11-25 10:53:09 +0700

    .env

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h2>สวัสดีค่า กินข้าวยังคะ</h2>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
